d7cb594b310519ea287e1263292afb7915ee09d2
TianSuoMuLong <hrm302056yyy@163.com>
2020-05-12 17:21:17 +0800

    版本同步

--- middleground/common_lib/src/test/java/com/common/util/CommonTest.java
+++ middleground/common_lib/src/test/java/com/common/util/CommonTest.java
@@ -1,7 +1,12 @@
 package com.common.util;
 
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 import org.junit.Test;
+
+import com.common.date.utils.DateUtil;
 
 public class CommonTest {
 
@@ -9,4 +14,16 @@ public class CommonTest {
 	public void get11() {
 		System.out.println("111111111");
 	}
+	@Test
+	public void datetest1() {
+		DateUtil du=new DateUtil();
+		String dateToString = du.dateToString(new Date(), "yyyyMMdd");
+		System.out.println(dateToString);
+		
+		
+		BigDecimal big=new BigDecimal(10000);
+		BigDecimal big2=new BigDecimal(10000.00);
+		System.out.println(big.compareTo(big2));
+	}
+	
 }
